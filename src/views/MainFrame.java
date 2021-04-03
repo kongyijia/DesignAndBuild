53483788cecacc81112c93d2e3b25eb2721d86cb
孙聚丰 <854591576@qq.com>
2021-04-03 18:24:39 +0800

    fea: frame

--- src/views/MainFrame.java
+++ src/views/MainFrame.java
@@ -6,34 +6,21 @@ public class MainFrame extends JFrame {
     public static final int WIDTH = 1200;
     public static final int HEIGHT = 600;
 
-    private JPanel loginPanel = new JPanel();
-    private MainPanel mainPanel = new MainPanel();
+    private MainPanel mainPanel;
 
     public MainFrame(){
         initialize();
-
-        loginPanel.setBounds(0,0,WIDTH, HEIGHT);
-        mainPanel.setBounds(0,0,WIDTH, HEIGHT);
-
-        this.add(mainPanel);
-        this.add(loginPanel);
     }
-    private void initialize() {
+
+    private void initialize(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle("CourseWork");
         this.setSize(WIDTH, HEIGHT);
         this.setLocationRelativeTo(null);
+
+        mainPanel = new MainPanel();
+        this.add(mainPanel);
+
         this.setVisible(true);
-        this.changeToLogin();
-    }
-
-    public void changeToMain(){
-        loginPanel.setVisible(false);
-        mainPanel.setVisible(true);
-    }
-
-    public void changeToLogin(){
-        loginPanel.setVisible(false);
-        mainPanel.setVisible(true);
     }
 }
--- src/views/MainPanel.java
+++ src/views/MainPanel.java
@@ -3,5 +3,46 @@ package views;
 import javax.swing.*;
 
 public class MainPanel extends JPanel {
+    public static final int WIDTH = 1200;
+    public static final int HEIGHT = 560;
 
+    private JPanel indexPanel;
+    private FunctionPanel functionPanel;
+    private int role = 0;
+
+    public MainPanel(){
+        initialize();
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+    private void initialize(){
+        this.setLayout(null);
+        this.setBounds(0,0, WIDTH, HEIGHT);
+        changeToFunction();
+    }
+
+    public void changeToFunction(){
+        if(functionPanel == null) {
+            this.functionPanel = new FunctionPanel(this, this.role);
+            this.add(functionPanel);
+            functionPanel.setVisible(true);
+        }
+        if(indexPanel != null) {
+            indexPanel.setVisible(false);
+        }
+    }
+
+    public void changeToIndex(){
+        if (functionPanel != null) {
+            functionPanel.setVisible(false);
+        }
+        if (indexPanel == null){
+            indexPanel = new IndexPanel(this);
+            this.add(indexPanel);
+            indexPanel.setVisible(true);
+        }
+    }
 }
